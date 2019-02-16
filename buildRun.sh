414ea498a937cd020c18678e01c8b11db04d8d7b
Kent Ogisu <kentogisu@gmail.com>
2019-02-16 00:40:25 -0500

    Refactoed buildRun.sh

--- buildRun.sh
+++ buildRun.sh
@@ -3,5 +3,5 @@ docker build -t paths-lb:latest .
 wait $PID
 echo "completed building docker image"
 echo "running docker image"
-docker run --rm -it -p 80:80 paths-lb
+docker run --name nginx -d -p 80:80 paths-lb
 
